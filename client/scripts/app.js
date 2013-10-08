01d7b245b779db53914c0b995cb514a3613ed027
Daniel <daniel.liebeskind@gmail.com>
2013-10-07 19:04:57 -0700

    Added roomname functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,8 @@
 (function(){
 
 var userName = window.location.search.split('=')[1];
+var currentRoom = 'lobby';
+var roomNames = {'lobby': true};
 
 var sendRequest = function(message){
   $.ajax({
@@ -12,7 +14,6 @@ var sendRequest = function(message){
       console.log('Message sent!');
     },
     error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message');
     }
   });
@@ -24,7 +25,7 @@ var getRequest = function(){
     type: 'GET',
     data: {
       order: '-createdAt',
-      limit: 20
+      limit: 100
     },
     contentType: 'application/json',
     success: function (data) {
@@ -40,15 +41,17 @@ var getRequest = function(){
 var parseMessages = function(messages) {
   $('.message').remove();
   for (var i = 0; i < messages.length; i++) {
-    var time = new Date(messages[i].createdAt);
-    var h = time.getHours();
-    var m = time.getMinutes();
-    // $('.messages').append('<div class = "message"> ' + '<span class = "createdAt"> ' + h + ':' + m + ' <span class = "username"> ' + messages[i].username + ' </span> ' + ' <span class = "text"> '  + messages[i].text + ' </span> </div>');
-    var $message = $('<div>').attr('class', 'message');
-    $message.append($('<span>').attr('class', 'createdAt').html(h + ':' + m + ' '));
-    $message.append($('<span>').attr('class', 'username').html(messages[i].username + ' '));
-    $message.append($('<span>').attr('class', 'text').html(messages[i].text));
-    $('.messages').append($message);
+    roomNames[messages[i].roomname] = true;
+    if (messages[i].roomname === currentRoom) {
+      var time = new Date(messages[i].createdAt);
+      var h = time.getHours();
+      var m = time.getMinutes();
+      var $message = $('<div>').attr('class', 'message');
+        $message.append($('<span>').attr('class', 'createdAt').html(h + ':' + m + ' '));
+        $message.append($('<span>').attr('class', 'username').html(messages[i].username + ' '));
+        $message.append($('<span>').attr('class', 'text').html(messages[i].text));
+        $('.messages').append($message);
+    }
   }
 };
 
@@ -56,10 +59,9 @@ setInterval(getRequest, 2000);
 
 $(document).on('click', 'button', function() {
   var submitted = $('input').val();
-  var message = {'username': userName, 'text': submitted, 'roomname': '4chan'};
+  var message = {'username': userName, 'text': submitted, 'roomname': currentRoom};
   sendRequest(message);
   $('input').val('');
 });
-
 }());
 
